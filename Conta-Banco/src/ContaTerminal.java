bdf7f17e5fb3f4d7c4db04752b345153215a8617
Chrysan TI <chrysan.costa@gmail.com>
2025-04-09 00:06:02 -0300

    começando o projeto

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       int numeroConta = 1021;
+       String agencia = "067-8";
+       String nomeCliente = "MARIO ANDRADE";
+       double saldo = 237.48;
+
+
+       
+
+    
+       /*System.out.println("Por favor digite o número da Agência: ");
+       System.out.println("Olá [nome do cliente], obrigado por criar uma conta em nosso banco, sua agência é 
+       [Agencia], conta [numero] e seu saldo [saldo] já está disponível para saque.");*/
     }
 }
